d18e47c968c577ceb6de98161dd9e14d67540ec8
MOSSAAB <mossaabmedj@gmail.com>
2025-02-14 21:27:59 +0100

    commit after pull?

--- WindowsFormApp-Project/obj/Debug/net8.0-windows/WindowsFormApp-Project.AssemblyInfo.cs
+++ WindowsFormApp-Project/obj/Debug/net8.0-windows/WindowsFormApp-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowsFormApp-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4f1f33c974bcf6bf7f3f037cce83eb5e7773a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f747a197190c0c0bb7825bda8e5f7d5328a55728")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowsFormApp-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowsFormApp-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
